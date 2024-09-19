de91fbb30aecba244a877913343400e65e8cda99
Samuel <samugit.projects@gmail.com>
2024-09-19 10:59:22 -0300

    Aula26_28 Backing bean binding componentes html jsf

--- mod26JSF/meuprimeiroprojetojsf/src/main/java/br/com/cursojsf/PessoaBean.java
+++ mod26JSF/meuprimeiroprojetojsf/src/main/java/br/com/cursojsf/PessoaBean.java
@@ -3,20 +3,38 @@ package br.com.cursojsf;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.component.html.HtmlCommandButton;
 
-@ApplicationScoped // agora independente de fechar o navegador, ou usuário encerrar, a informação é compartilhada com todos na aplicação e mantido em tela.
+@ViewScoped
 @ManagedBean(name = "pessoaBean")
 public class PessoaBean {
 
 	private String nome;
 	
+	private HtmlCommandButton commandButton; // como nossa classe possui um Bean gerenciado, de qualquer tipo, nesse caso o ViewScoped, podemos fazer uma ponte (binding) e controlar um componente do front end, nesse exemplo o botão
+	// Não é muito recomendado fazer isso pois, entre outras coisas, ocorre o acoplamento do componente da interface à lógica do bean.
+	// Isso pode dificultar a manutenção e os testes, haverá necessidade de garantir que o bean sempre tenha acesso ao componente.
+	
 	private List<String> nomes = new ArrayList<>() ;
 
 	public String addNome() {
 		nomes.add(nome);
+		
+		if (nomes.size() > 3) {
+			commandButton.setDisabled(true); // por exemplo se lançar mais de três nomes o botão de lançamento é desabilitado.
+		}
+		
 		return "";
+	}
+	
+	public void setCommandButton(HtmlCommandButton commandButton) {
+		this.commandButton = commandButton;
+	}
+	
+	public HtmlCommandButton getCommandButton() {
+		return commandButton;
 	}
 	
 	public void setNomes(List<String> nomes) {
