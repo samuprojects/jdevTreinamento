9c85359ed2daef735c40da6e51ca12fce093111e
Samuel <samugit.projects@gmail.com>
2024-09-25 15:16:46 -0300

    Aula27_09 Criando DAO Genérico

---
+++ mod27_ORM_JPA_Hibernate/pos-java-maven-hibernate/src/main/java/dao/DaoGeneric.java
@@ -0,0 +1,18 @@
+package dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import posjavamavenhibernate.HibernateUtil;
+
+public class DaoGeneric<E> {
+	
+	private EntityManager entityManager = HibernateUtil.getEntityManager();
+	
+	public void salvar(E entidade) {
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+		entityManager.persist(entidade);
+		transaction.commit();
+	}
+}
--- mod27_ORM_JPA_Hibernate/pos-java-maven-hibernate/src/test/java/posjavamavenhibernate/TesteHibernate.java
+++ mod27_ORM_JPA_Hibernate/pos-java-maven-hibernate/src/test/java/posjavamavenhibernate/TesteHibernate.java
@@ -2,10 +2,24 @@ package posjavamavenhibernate;
 
 import org.junit.Test;
 
+import dao.DaoGeneric;
+import model.UsuarioPessoa;
+
 public class TesteHibernate {
 
 	@Test
 	public void testeHibernateUtil() {
-		HibernateUtil.getEntityManager();
+		DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<UsuarioPessoa>();
+		
+		UsuarioPessoa pessoa = new UsuarioPessoa();
+		
+		pessoa.setIdade(45);
+		pessoa.setLogin("teste");
+		pessoa.setNome("Sam Dev");
+		pessoa.setSenha("123");
+		pessoa.setSobrenome("Java");
+		pessoa.setEmail("samdev@example.com");
+		
+		daoGeneric.salvar(pessoa);
 	}
 }
