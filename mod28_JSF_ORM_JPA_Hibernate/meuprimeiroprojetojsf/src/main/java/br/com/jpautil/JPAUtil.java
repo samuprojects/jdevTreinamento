23cab4d3848058d9fd6df184afb1aacc8db8b8b8
Samuel <samugit.projects@gmail.com>
2024-10-01 10:50:32 -0300

    Aula28_10 Classe JPAUtil

---
+++ mod28_JSF_ORM_JPA_Hibernate/meuprimeiroprojetojsf/src/main/java/br/com/jpautil/JPAUtil.java
@@ -0,0 +1,22 @@
+package br.com.jpautil;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAUtil {
+	
+	private static EntityManagerFactory factory;
+	
+	static {
+		
+		if (factory == null) {
+			factory = Persistence.createEntityManagerFactory("meuprimeiroprojetojsf");
+		}
+	}
+	
+	public static EntityManager getEntityManager() {
+		return factory.createEntityManager();
+	}
+
+}
