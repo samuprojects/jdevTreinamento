68010035f0814245aa2b270813a4814c5aec1557
Samuel <samugit.projects@gmail.com>
2024-10-02 15:27:08 -0300

    Aula28_15 Delete com JPA e JSF

--- mod28_JSF_ORM_JPA_Hibernate/meuprimeiroprojetojsf/src/main/java/br/com/jpautil/JPAUtil.java
+++ mod28_JSF_ORM_JPA_Hibernate/meuprimeiroprojetojsf/src/main/java/br/com/jpautil/JPAUtil.java
@@ -18,5 +18,9 @@ public class JPAUtil {
 	public static EntityManager getEntityManager() {
 		return factory.createEntityManager();
 	}
+	
+	public static Object getPrimaryKey(Object entity) {
+		return factory.getPersistenceUnitUtil().getIdentifier(entity);
+	}
 
 }
