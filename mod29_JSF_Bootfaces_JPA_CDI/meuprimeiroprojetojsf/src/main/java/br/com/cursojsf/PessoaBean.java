4c3658b3805d7d7a97f992f519d0e7288edd0308
Samuel <samugit.projects@gmail.com>
2024-10-07 10:36:20 -0300

    Aula29_13 Criando a rotina de login e autenticando com o banco de dados

--- mod29_JSF_Bootfaces_JPA_CDI/meuprimeiroprojetojsf/src/main/java/br/com/cursojsf/PessoaBean.java
+++ mod29_JSF_Bootfaces_JPA_CDI/meuprimeiroprojetojsf/src/main/java/br/com/cursojsf/PessoaBean.java
@@ -6,9 +6,13 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 
 import br.com.dao.DaoGeneric;
 import br.com.entidades.Pessoa;
+import br.com.repository.IDaoPessoa;
+import br.com.repository.IDaoPessoaImpl;
 
 @ViewScoped
 @ManagedBean(name = "pessoaBean")
@@ -17,6 +21,8 @@ public class PessoaBean {
 	private Pessoa pessoa = new Pessoa();
 	private DaoGeneric<Pessoa> daoGeneric = new DaoGeneric<Pessoa>();
 	private List<Pessoa> pessoas = new ArrayList<Pessoa>();
+	
+	private IDaoPessoa iDaoPessoa = new IDaoPessoaImpl();
 
 	public String salvar() {
 		pessoa = daoGeneric.merge(pessoa);
@@ -59,6 +65,23 @@ public class PessoaBean {
 	
 	public List<Pessoa> getPessoas() {
 		return pessoas;
+	}
+	
+	public String logar() {
+		
+		Pessoa pessoaUser = iDaoPessoa.consultarUsuario(pessoa.getLogin(), pessoa.getSenha());
+		
+		if (pessoaUser != null) { // encontrou usuario
+			
+			// adicionar usuário na sessão usuarioLogado
+			FacesContext context = FacesContext.getCurrentInstance();
+			ExternalContext externalContext = context.getExternalContext();
+			externalContext.getSessionMap().put("usuarioLogado", pessoaUser.getLogin());
+			
+			return "primeirapagina.jsf";
+		}
+		
+		return "index.jsf";
 	}
 
 }
