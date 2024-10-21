8f33d385b7b4ae12af1be88e38edd5405e68fb7e
Samuel <samugit.projects@gmail.com>
2024-10-21 09:34:40 -0300

    Aula29_43 Criando a camada de modelo para upload

--- mod29_JSF_Bootfaces_JPA_CDI/meuprimeiroprojetojsf/src/main/java/br/com/entidades/Pessoa.java
+++ mod29_JSF_Bootfaces_JPA_CDI/meuprimeiroprojetojsf/src/main/java/br/com/entidades/Pessoa.java
@@ -4,10 +4,14 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
+import javax.persistence.Basic;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -75,6 +79,39 @@ public class Pessoa implements Serializable {
 	@Transient // anotação para não realizar a persistência no banco de dados, apenas na memória.
 	private Estados estados;
 	
+	@Column(columnDefinition = "text") // gravar arquivos em base 64
+	private String fotoIconBase64;
+	
+	private String extensao; // para gravar extensão do arquivo (jpg, png, etc).
+	
+	@Lob // para garavar arquivo no banco de dados
+	@Basic(fetch = FetchType.LAZY)
+	private byte[] fotoIconBase64Original;
+	
+	public String getFotoIconBase64() {
+		return fotoIconBase64;
+	}
+
+	public void setFotoIconBase64(String fotoIconBase64) {
+		this.fotoIconBase64 = fotoIconBase64;
+	}
+
+	public String getExtensao() {
+		return extensao;
+	}
+
+	public void setExtensao(String extensao) {
+		this.extensao = extensao;
+	}
+
+	public byte[] getFotoIconBase64Original() {
+		return fotoIconBase64Original;
+	}
+
+	public void setFotoIconBase64Original(byte[] fotoIconBase64Original) {
+		this.fotoIconBase64Original = fotoIconBase64Original;
+	}
+
 	public void setEstados(Estados estados) {
 		this.estados = estados;
 	}
