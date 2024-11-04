474c7d921c0d95cfe5dc7db85e83025dcc502f1d
Samuel <samugit.projects@gmail.com>
2024-11-04 14:56:35 -0300

    Aula30_23 Passando parâmetros para outras telas

---
+++ mod30_JSF_Hibernate_ORM/pos-java-maven-hibernate/src/main/java/managedBean/TelefoneManagedBean.java
@@ -0,0 +1,32 @@
+package managedBean;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+import dao.DaoUsuario;
+import model.UsuarioPessoa;
+
+@ManagedBean(name = "telefoneManagedBean")
+@ViewScoped
+public class TelefoneManagedBean {
+	
+	private UsuarioPessoa user = new UsuarioPessoa();
+	private DaoUsuario<UsuarioPessoa> daoUser = new DaoUsuario<UsuarioPessoa>();
+
+	@PostConstruct
+	public void init () {
+		
+		String coduser = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("codigouser");
+		user = daoUser.pesquisar(Long.parseLong(coduser), UsuarioPessoa.class);
+	}
+	
+	public void setUser(UsuarioPessoa user) {
+		this.user = user;
+	}
+	
+	public UsuarioPessoa getUser() {
+		return user;
+	}
+}
