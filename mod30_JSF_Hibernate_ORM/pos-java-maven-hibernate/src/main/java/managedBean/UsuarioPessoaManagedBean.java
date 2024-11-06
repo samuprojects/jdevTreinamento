81f46503fee1412e2acfc656673fa327ea054ee4
Samuel <samugit.projects@gmail.com>
2024-11-06 17:55:34 -0300

    Aula30_26 Consumindo Web Service CEP RESTful em JSF - Aula Única

--- mod30_JSF_Hibernate_ORM/pos-java-maven-hibernate/src/main/java/managedBean/UsuarioPessoaManagedBean.java
+++ mod30_JSF_Hibernate_ORM/pos-java-maven-hibernate/src/main/java/managedBean/UsuarioPessoaManagedBean.java
@@ -1,5 +1,10 @@
 package managedBean;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,6 +13,9 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
+import javax.faces.event.AjaxBehaviorEvent;
+
+import com.google.gson.Gson;
 
 import dao.DaoUsuario;
 import model.UsuarioPessoa;
@@ -23,6 +31,37 @@ public class UsuarioPessoaManagedBean {
 	@PostConstruct
 	public void init( ) {
 		list = daoGeneric.listar(UsuarioPessoa.class);
+	}
+	
+	public void pesquisaCep(AjaxBehaviorEvent event) {
+		try {
+
+			URL url = new URL("https://viacep.com.br/ws/" + usuarioPessoa.getCep() + "/json/");
+			URLConnection connection = url.openConnection();
+			InputStream is = connection.getInputStream();
+			BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+			
+			String cep = "";
+			StringBuilder jsonCep = new StringBuilder();
+			
+			while ((cep = br.readLine()) != null ) {
+				jsonCep.append(cep);
+			}
+				
+			UsuarioPessoa userCepPessoa = new Gson().fromJson(jsonCep.toString(), UsuarioPessoa.class);
+			
+			usuarioPessoa.setCep(userCepPessoa.getCep());
+			usuarioPessoa.setLogradouro(userCepPessoa.getLogradouro());
+			usuarioPessoa.setComplemento(userCepPessoa.getComplemento());
+			usuarioPessoa.setBairro(userCepPessoa.getBairro());
+			usuarioPessoa.setLocalidade(userCepPessoa.getLocalidade());
+			usuarioPessoa.setUf(userCepPessoa.getUf());
+			usuarioPessoa.setIbge(userCepPessoa.getIbge());
+			usuarioPessoa.setGia(userCepPessoa.getGia());
+					
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public UsuarioPessoa getUsuarioPessoa() {
