3f8a9ec8a4a84ebb25f5a3f62ba2a8c55e1df961
Samuel <samugit.projects@gmail.com>
2024-11-20 13:37:28 -0300

    Aula31_21 Criando a interface de CRUD ActionViewPadrao

---
+++ mod31_JSF_PrimeFaces_Spring_Hibernate/aulasjsfprimefaces/src/main/java/br/com/project/util/all/ActionViewPadrao.java
@@ -0,0 +1,36 @@
+package br.com.project.util.all;
+
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+
+public interface ActionViewPadrao extends Serializable {
+	
+	abstract void limparLista() throws Exception;
+	
+	abstract String save() throws Exception;
+	
+	abstract void saveNotReturn() throws Exception;
+	
+	abstract void saveEdit() throws Exception;
+	
+	/**
+	 * @PostConstruct realizar inicialização de métodos, valores ou variáveis
+	 * @throws Exception
+	 */
+	@PostConstruct
+	abstract String novo() throws Exception;
+	
+	abstract void definirVariaveisNulas() throws Exception;
+	
+	abstract void consultarEntidade() throws Exception;
+	
+	abstract void statusOperation(SituacaoPersistencia a) throws Exception;
+	
+	abstract String redirecionarNewEntidade() throws Exception;
+	
+	abstract String redirecionarFindEntidade() throws Exception;
+	
+	abstract void addMsg(String msg);
+
+}
