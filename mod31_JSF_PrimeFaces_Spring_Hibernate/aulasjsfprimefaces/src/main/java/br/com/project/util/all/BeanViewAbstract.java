b3064c52be8ab5409bd143cbbb81f372ed877fde
Samuel <samugit.projects@gmail.com>
2024-11-21 15:01:35 -0300

    Aula31_22 Criando a abstracação para os ManagedBeans JSF

---
+++ mod31_JSF_PrimeFaces_Spring_Hibernate/aulasjsfprimefaces/src/main/java/br/com/project/util/all/BeanViewAbstract.java
@@ -0,0 +1,71 @@
+package br.com.project.util.all;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public abstract class BeanViewAbstract implements ActionViewPadrao {
+
+	@Override
+	public void limparLista() throws Exception {
+		
+	}
+
+	@Override
+	public String save() throws Exception {
+		return null;
+	}
+
+	@Override
+	public void saveNotReturn() throws Exception {
+		
+	}
+
+	@Override
+	public void saveEdit() throws Exception {
+		
+	}
+
+	@Override
+	public String novo() throws Exception {
+		return null;
+	}
+
+	@Override
+	public void definirVariaveisNulas() throws Exception {
+		
+	}
+
+	@Override
+	public void consultarEntidade() throws Exception {
+		
+	}
+
+	@Override
+	public void statusOperation(SituacaoPersistencia a) throws Exception {
+		Mensagens.responseOperation(a);
+	}
+	
+	protected void sucesso() throws Exception {
+		statusOperation(SituacaoPersistencia.SUCESSO);
+	}
+	
+	protected void error() throws Exception {
+		statusOperation(SituacaoPersistencia.ERRO);
+	}
+
+	@Override
+	public String redirecionarNewEntidade() throws Exception {
+		return null;
+	}
+
+	@Override
+	public String redirecionarFindEntidade() throws Exception {
+		return null;
+	}
+
+	@Override
+	public void addMsg(String msg) {
+		Mensagens.msg(msg);
+	}
+
+}
