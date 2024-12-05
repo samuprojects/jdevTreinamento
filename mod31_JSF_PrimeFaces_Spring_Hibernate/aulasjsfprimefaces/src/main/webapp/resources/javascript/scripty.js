bb1243a448959a124828e581988abf3eb625a239
Samuel <samugit.projects@gmail.com>
2024-12-05 13:56:22 -0300

    Aula31_67 Criando Template inicial - Parte 9

--- mod31_JSF_PrimeFaces_Spring_Hibernate/aulasjsfprimefaces/src/main/webapp/resources/javascript/scripty.js
+++ mod31_JSF_PrimeFaces_Spring_Hibernate/aulasjsfprimefaces/src/main/webapp/resources/javascript/scripty.js
@@ -1,3 +1,38 @@
+var arrayIdElementsPage = new Array;
+
+/**
+ * Carregar um array global com os ids de todos os componentes da pagina para ter 
+ * facilidades na obtenção de valores dos componentes, além de trabalhar com ajax
+ */
+function carregarIdElementosPagina(){
+	arrayIdElementsPage = new Array;
+	for (form = 0; form <= document.forms.length; form++) {
+		var formAtual = document.forms[form];
+		if (formAtual != undefined) {
+			for (i = 0; i < document.forms[form].elements.length; i++){
+				if (document.forms[form].elements[i].id != '') {
+					arrayIdElementsPage[i] = document.forms[form].elements[i].id;
+				}
+			}
+		}
+	}
+}
+
+/**
+ * Retornar valor do id do componente dentro do documento html passando como parâmetro a descrição do id declarada em jsf
+ * 
+ * @param id
+ */
+function getValorElementPorId(id) {
+	carregarIdElementosPagina();
+	for (i = 0; i < arrayIdElementsPage.length; i++) {
+		var valor = "" + arrayIdElementsPage[i];
+		if (valor.indexOf(id) > -1) {
+			return valor;
+		}
+	}
+	return idundefined;
+}
 
 function logout(contextPath) {
 	
@@ -56,3 +91,58 @@ function redirecionarPagina(context, pagina) {
 	pagina = pagina + ".jsf";
 	document.location = context + pagina;
 }
+
+function localeData_pt_br() {
+	PrimeFaces.locales['pt'] = {
+		closeText : 'Fechar',
+		prevText : 'Anterior',
+		nextText : 'Próximo',
+		currentText : 'Começo',
+		monthNames : [ 'Janeiro', 'Fevereiro', 'Marcio', 'Abril', 'Maio',
+				'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro',
+				'Dezembro' ],
+		monthNamesShort : [ 'Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul',
+				'Ago', 'Set', 'Out', 'Nov', 'Dez' ],
+		dayNames : [ 'Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta',
+				'Sexta', 'Sábado' ],
+		dayNamesShort : [ 'Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb' ],
+		dayNamesMin : [ 'D', 'S', 'T', 'Q', 'Q', 'S', 'S' ],
+		weekHeader : 'Semana',
+		firstDay : 0,
+		isRTL : false,
+		showMonthAfterYear : false,
+		yearSuffix : '',
+		timeOnlyTitle : 'São Horas',
+		timeText : 'Tempo',
+		hourText : 'Hora',
+		minuteText : 'Minuto',
+		secondText : 'Segundo',
+		ampm : false,
+		month : 'Mês',
+		week : 'Semana',
+		day : 'Dia',
+		allDayText : 'Todo o Dia'
+	};
+}
+
+function initTemplate() {
+	$(document).ready(function() {
+		  $('#menupop').hide();
+		  $('#barramenu').hide();
+		  $('#barramenu').css("left", "-200px");
+		  $('#iniciarmenu').click(function() {
+		  	if ($('#barramenu').is(':visible')) {
+		  	  ocultarMenu();
+		  	} else {
+		  	  $('#barramenu').show();
+		  	  $('#barramenu').animate({"left":"0px"}, "slow");	
+		  	}
+		  });
+		});
+	}
+
+function ocultarMenu() {  
+	  $('#barramenu').animate({"left":"-200px"}, "slow", function() {
+	  	$('#barramenu').hide();
+	  });
+	}
